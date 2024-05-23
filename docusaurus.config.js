d9784e3c1a1be91620f691bd84dadcb56cd72f79
alexcss <kalyuzhnyi@gmail.com>
2024-05-23 16:41:33 +0300

    Concept content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   organizationName: 'rsksmart', // Usually your GitHub org/user name.
   projectName: 'devportal', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
