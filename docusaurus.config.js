a2d4b994eb6d73292f6d4f2d0dcb6ba1aedb66a8
alexcss <kalyuzhnyi@gmail.com>
2024-06-13 11:44:07 +0300

    Change site url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://dev.rootstock.io',
+  url: 'https://dev-rootstock.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
