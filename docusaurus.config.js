c050e4a1b1e954ec6d03c8fb2cb9d6c7e1b4056d
alexcss <kalyuzhnyi@gmail.com>
2024-06-20 16:24:39 +0300

    Fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zgraya-digital/devportal-rootstock/tree/main/',
         },
         blog: false, // Optional: disable the blog plugin
         theme: {
