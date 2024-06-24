beb9c628e853e39dc48ea5fc32c2cc1b75fa79cc
alexcss <kalyuzhnyi@gmail.com>
2024-06-24 15:20:44 +0300

    Homepage - guides section

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import clsx from "clsx";
 import TitleColor from "../TitleColor";
+import Link from "@docusaurus/core/lib/client/exports/Link";
+import styles from "./styles.module.scss";
 
-export default function Card ({title, description, list, color = "orange", children, className, ...props}) {
+export default function Card({title, description, list, color = "orange", children, className, ...props}) {
 
-  return <div className={clsx(`border rounded-20 px-16 py-24 p-lg-32 h-100`, className)} >
+  return <div className={clsx(`border d-flex flex-column rounded-20 px-16 py-24 p-lg-32 h-100`, className, styles.Card)} style={{'--rsk-card-main-color' : `var(--bs-${color})`}}>
     <TitleColor as={'h3'} size={'sm'} color={color} className={`mb-16`}>
       {title}
     </TitleColor>
-    <div className={`text-block`}>
+    <div className={`markdown fw-medium`}>
       {description}
     </div>
+    {list && (
+      <ul className={`mb-0 mt-auto pt-20`}>
+        {list.map((item, idx) => (
+          <li key={idx}>
+            <Link href={item.href} target={item.target}>
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )}
   </div>
 }
--- src/components/HomepageSectionGuides/index.js
+++ src/components/HomepageSectionGuides/index.js
@@ -13,6 +13,17 @@ const GuidesList = [
         See the ways you can get started.
       </>
     ),
+    list: [
+      {
+        title: 'Explore Core Concepts',
+        href: '#',
+      },
+      {
+        title: 'Deploy your First DApp on Rootstock',
+        href: '#',
+      },
+    ],
+
   },
   {
     title: 'Developers',
@@ -22,6 +33,17 @@ const GuidesList = [
         Start building and integrating DApps
       </>
     ),
+    list: [
+      {
+        title: 'Deploy Smart Contracts',
+        href: '#',
+      },
+      {
+        title: 'Quick start guide with Hardhat',
+        href: '#',
+      },
+    ],
+
   },
   {
     title: 'Node Operators',
@@ -31,35 +53,32 @@ const GuidesList = [
         Are you a node miner? Interested in merged mining on Rootstock? Get up and running with Rootstock nodes or use the RPC API.
       </>
     ),
+    list: [
+      {
+        title: 'Run a Node',
+        href: '#',
+      },
+      {
+        title: 'RPC API',
+        href: '#',
+      },
+    ],
+
   },
 ];
-
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={``} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
 export default function HomepageSectionGuides() {
   return (
     <section className={`mb-64`}>
 
-      <h2 className={`h1`}>Guides for Builders</h2>
-      <div className="text-block">
+      <h2 className={`h1 mb-12`}>Guides for Builders</h2>
+      <div className="markdown mb-24">
         <p>Whether you're a developer building a dApp Rootstock, a node miner running a merged mining on Rootstock, or a general user of tools and services, you'll find everything you need to get started right here.</p>
       </div>
       <div className="row row-cols-1 row-cols-lg-3">
         {GuidesList.map((item, idx) => (
           <div className={`col`} key= {idx}>
-            <Card title={item.title} color={item.color} description={item.description} />
+            <Card title={item.title} color={item.color} description={item.description} list={item.list} />
           </div>
         ))}
       </div>
