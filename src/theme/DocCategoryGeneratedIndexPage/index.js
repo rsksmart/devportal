6bd7dcb53bd15b3f45b74bd64c4e2853c1bcc6d0
alexcss <kalyuzhnyi@gmail.com>
2024-07-07 16:46:42 +0300

    Category index file

---
+++ src/theme/DocCategoryGeneratedIndexPage/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  PageMetadata,
+  useCurrentSidebarCategory,
+} from '@docusaurus/theme-common';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import DocCardList from '@theme/DocCardList';
+import DocPaginator from '@theme/DocPaginator';
+import DocVersionBanner from '@theme/DocVersionBanner';
+import DocVersionBadge from '@theme/DocVersionBadge';
+import DocBreadcrumbs from '@theme/DocBreadcrumbs';
+import Heading from '@theme/Heading';
+
+import TitleColor from "/src/components/TitleColor";
+
+import styles from './styles.module.css';
+import clsx from "clsx";
+function DocCategoryGeneratedIndexPageMetadata({categoryGeneratedIndex}) {
+  return (
+    <PageMetadata
+      title={categoryGeneratedIndex.title}
+      description={categoryGeneratedIndex.description}
+      keywords={categoryGeneratedIndex.keywords}
+      // TODO `require` this?
+      image={useBaseUrl(categoryGeneratedIndex.image)}
+    />
+  );
+}
+function DocCategoryGeneratedIndexPageContent({categoryGeneratedIndex}) {
+  const category = useCurrentSidebarCategory();
+  return (
+    <div className={clsx(styles.generatedIndexPage, 'ps-lg-24')}>
+      <div className="mb-24">
+        <DocVersionBanner />
+        <DocBreadcrumbs />
+        <DocVersionBadge />
+      </div>
+      <header className={`mb-24`}>
+        <Heading as="h1" className={`h1 mb-26`}>
+          {categoryGeneratedIndex.title}
+        </Heading>
+        {categoryGeneratedIndex.description && (
+          <p>{categoryGeneratedIndex.description}</p>
+        )}
+      </header>
+      <DocCardList items={category.items} className={styles.list} />
+
+      <footer>
+        <DocPaginator
+          previous={categoryGeneratedIndex.navigation.previous}
+          next={categoryGeneratedIndex.navigation.next}
+        />
+      </footer>
+    </div>
+  );
+}
+export default function DocCategoryGeneratedIndexPage(props) {
+  return (
+    <>
+      <DocCategoryGeneratedIndexPageMetadata {...props} />
+      <DocCategoryGeneratedIndexPageContent {...props} />
+    </>
+  );
+}
