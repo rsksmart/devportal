ffd48d30373ed43cc7532119439f44511919856c
alexcss <kalyuzhnyi@gmail.com>
2024-11-08 12:49:15 +0200

    Add align option for Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Link from '/src/components/Link';
 import clsx from "clsx";
 
-export default function Button ({variant, href, className, size, target, title, icon, stretched, children, ...props}) {
-  const btnClassName = clsx(`btn`, className, size && `btn-${size}`, stretched && `stretched-link position-static`);
+export default function Button ({variant, href, className, size, target, title, icon, stretched, align, children, ...props}) {
+  const btnClassName = clsx(`btn`, className, size && `btn-${size}`, stretched && `stretched-link position-static`, align === 'left' && `d-flex w-fit`, align === 'center' && `d-flex w-fit mx-auto`, align === 'right' && `d-flex w-fit ms-auto`)
   // will start with exactly one slash, and that anything else is external.
   const Icon = () => {
     return icon && (
