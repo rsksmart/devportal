983571ac2ab94507fa86d718c0e40822613db650
alexcss <kalyuzhnyi@gmail.com>
2024-11-12 16:17:36 +0200

    Peg in Address Verifier add result styles

--- src/components/AddressVerifier/index.js
+++ src/components/AddressVerifier/index.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { getAddressInformation, canPegIn } from '/src/_utils/pegin-address-verifier/pegin-address-verifier'
+import Admonition from '@theme/Admonition'
 
 export default function AddressVerifier () {
 
   const [address, setAddress] = useState('')
   const [addressInfo, setAddressInfo] = useState('')
   const inputRef = useRef(null)
+  const clearForm = (e) => {
+    setAddress('')
+    setAddressInfo('')
+  }
   const checkAddress = (e) => {
     e.preventDefault()
-    const address = inputRef.current.value;
+    if (!inputRef?.current?.value) return
+
+    const address = inputRef.current.value.trim()
     setAddress(address)
     setAddressInfo(getAddressInformation(address))
   }
 
   const AddressInfoResult = ({ info }) => {
     const [result, setResult] = useState('')
+    const [resultType, setResultType] = useState('')
+    const [resultTitle, setResultTitle] = useState('')
 
     useEffect(() => {
-      const displayAddress = `<code>${address}</code>`;
+      const displayAddress = `<code>${address}</code>`
 
       if (info === null) {
         setResult(`The address ${displayAddress} is not valid.`)
+        setResultType('warning')
+        setResultTitle('Not valid')
       } else if (info?.type) {
+        setResultType('success')
+        setResultTitle('Valid')
         let displayAddressType = `<code>${info.type.toUpperCase()}</code>`
         let displayNetwork = `<code>${info.network.charAt(0).toUpperCase()}${info.network.slice(1)}</code>`
 
@@ -39,15 +52,20 @@ export default function AddressVerifier () {
     }, [info])
 
     return result && (
-      <div className="pt-12" dangerouslySetInnerHTML={{ __html: result }}></div>
+      <Admonition title={resultTitle} type={resultType}>
+        <div dangerouslySetInnerHTML={{ __html: result }}></div>
+      </Admonition>
     )
   }
 
   return (
     <form className="pegin-address-verifier" onSubmit={checkAddress}>
-      <div className="d-flex flex-column gap-12 align-items-start flex-md-row align-items-md-stretch">
+      <div className="d-flex flex-column gap-12 align-items-start flex-md-row align-items-md-center">
         <input type="text" className="form-control form-control--xl" required={true} ref={inputRef}/>
-        <button type="submit" className="btn ">Check</button>
+        <div className="d-flex gap-12">
+          <button type="submit" className="btn">Check</button>
+          <button type="reset" className="btn" onClick={clearForm}>Clear</button>
+        </div>
       </div>
       <AddressInfoResult info={addressInfo}/>
     </form>
