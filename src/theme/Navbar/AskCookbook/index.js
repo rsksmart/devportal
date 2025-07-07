a2fc6852c8bc1553d2ae7885f118dbd32527bad5
max-barrera-2148 <max.barrera@rootstocklabs.com>
2025-07-07 09:40:44 -0500

    Ask Cookbook button click tracking

--- src/theme/Navbar/AskCookbook/index.js
+++ src/theme/Navbar/AskCookbook/index.js
@@ -5,17 +5,26 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 const BaseAskCookbook = React.lazy(() =>
   import("@cookbookdev/docsbot/react-fixed")
 );
+
 export default function AskCookbook() {
   const {
-        siteConfig: {customFields},
-      } = useDocusaurusContext();
-      const {keys} = customFields;
-      const COOKBOOK_PUBLIC_API_KEY = keys.cookbook;
+    siteConfig: { customFields },
+  } = useDocusaurusContext();
+  const { keys } = customFields;
+  const COOKBOOK_PUBLIC_API_KEY = keys.cookbook;
+
   return (
     <BrowserOnly>
       {() => (
         <Suspense>
-          <BaseAskCookbook apiKey={COOKBOOK_PUBLIC_API_KEY} />
+          <BaseAskCookbook
+            apiKey={COOKBOOK_PUBLIC_API_KEY}
+            onOpen={() => {
+              window.dataLayer = window.dataLayer || [];
+              window.dataLayer.push({ event: "askCookbookClick" });
+              console.log("✅ askCookbookClick event fired from onOpen prop");
+            }}
+          />
         </Suspense>
       )}
     </BrowserOnly>
